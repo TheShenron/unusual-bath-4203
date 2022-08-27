a8502d9638e953cb920d8aff883f41c2c5d41b4c
dcs1997 <dhirajsalvi099@gmail.com>
2022-08-28 01:15:47 +0530

    fw20_1177  header/footer added

--- script/main.js
+++ script/main.js
@@ -3,3 +3,9 @@ console.log("Working...")
 import Navbar from "./fw20_1177/Navbar.js"
 
 document.querySelector("#Nav").innerHTML = Navbar()
+
+
+
+import Footer from "./fw20_1177/Footer.js"
+
+document.querySelector("#real_footer").innerHTML = Footer()
