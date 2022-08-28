f65cf619869f3443ce41640ec98b29dd5123c040
theshenron <Gourav143faz@gmail.com>
2022-08-28 09:59:31 +0530

    fw20_0020 AD

--- script/fw20_0020/d3.js
+++ script/fw20_0020/d3.js
@@ -2,11 +2,42 @@ console.log("fw20_0020...Signin page")
 
 
 document.querySelector("input[type='email']").addEventListener("focus" , FormMagic)
+document.querySelector("input[type='password']").addEventListener("focus" , FormMagic)
 
 function FormMagic () {
-    console.log(this.id + " spna")
-    // let x = document.querySelector(`#${this.id} + span`)
-    // console.log(x)
+
+    let x = document.querySelector(`#${this.id} + span`)
+    x.style.transform = "translateX(13px) translateY(-10px)"
+    x.style.fontSize = "15px"
+    
 }
 
-//not completd yet working....
+
+document.querySelector("input[type='email']").addEventListener("focusout" , FormRemoveMagic)
+document.querySelector("input[type='password']").addEventListener("focusout" , FormRemoveMagic)
+
+function FormRemoveMagic () {
+
+    let x = document.querySelector(`#${this.id} + span`)
+    if(this.value == ""){
+        x.style.transform = "translateX(10px) translateY(15px)"
+        x.style.fontSize = "18px"
+    }
+
+}
+
+
+document.querySelector("#SignIn").addEventListener("click" , SignIn)
+
+function SignIn () {
+    console.log("ASFD")
+    let email = document.querySelector("#LOG").value
+
+    let password = document.querySelector("#PAS").value
+
+    if(email!= "" && password != ""){
+        alert("Sign in Successfully")
+    }else{
+        alert("Fill all the fields")
+    }
+}
