5d4a3509111fd83569470d3e835b706f99b7c62d
theshenron <Gourav143faz@gmail.com>
2022-08-29 08:57:32 +0530

    Working on navbar

--- script/main.js
+++ script/main.js
@@ -2,10 +2,35 @@ console.log("Working...")
 
 import Navbar from "./fw20_1177/Navbar.js"
 
-document.querySelector("#Nav").innerHTML = Navbar()
+// document.querySelector("#Nav").innerHTML = Navbar()
 
 
 
 import Footer from "./fw20_1177/Footer.js"
 
-document.querySelector("#real_footer").innerHTML = Footer()
+// document.querySelector("#real_footer").innerHTML = Footer()
+
+
+
+document.querySelector("#nav_data").classList.add("magin")
+
+
+document.querySelector(".hamburger").addEventListener("click", () => {
+    document.querySelector("#parent_Div").classList.toggle("move")
+    document.querySelector("#nav_data").classList.toggle("magin")
+
+
+})
+
+
+
+
+let lis = document.querySelectorAll("#nav_data > div>ul>li ")
+console.log(lis)
+
+lis.forEach((el) => {
+    console.log(el.children)
+    el.children[0].addEventListener("click", () => {
+        el.children[1].classList.toggle("Height");
+    });
+});
